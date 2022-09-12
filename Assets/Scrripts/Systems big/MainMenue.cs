221f2eea3a5bdca30e7110ce95d3750bfe540695
Noah <deremo.noah@gmail.com>
2022-09-11 23:06:02 -0500

    Goal Reached!
    tutorial finished thanks cody

---
+++ Assets/Scrripts/Systems big/MainMenue.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenue : MonoBehaviour
+{
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void TutorialThenGame()
+    {
+        StartCoroutine(TutorialRoutine());
+    }
+    public IEnumerator TutorialRoutine()
+    {
+        yield return SceneManager.LoadSceneAsync(1,LoadSceneMode.Additive);
+        Debug.Log("got here");
+        FindObjectOfType<TutorialManager>().yesToTutorial();
+        yield return SceneManager.UnloadSceneAsync(0);
+    }
+}
---
+++ Assets/Scrripts/Systems big/TutorialManager.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField] GameObject tutorialPanel;
+    [SerializeField] Text tutorialText;
+    [SerializeField] Sprite SensaiSprite;
+    private bool _tutorialing;
+    private TutorialState _tutorialState = TutorialState.tohold;
+    private enemy TrainingDummy;
+    private float _timer = 1;
+    private EnemysManager _enemyManager;
+    bool buttonInput => Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Mouse0);
+
+    IEnumerator Start()
+    {
+        _enemyManager = GetComponent<EnemysManager>();
+        if (_tutorialing)
+        {
+            _enemyManager.enemyWaves.Insert(0, Resources.Load<EnmWave>("Waves/Tutorial Wave"));
+            Debug.Log("if");
+        }
+        else
+        {
+            yield break;
+        }
+        Debug.Log("started tutorial");
+
+        yield return new WaitForSeconds(1);
+        tutorialPanel.SetActive(true);
+        tutorialText.text = "Hold Down the Space bar or Left Mouse button";
+        
+        while(_tutorialState == TutorialState.tohold)
+        {
+            while (buttonInput == false)
+            { 
+                yield return null; 
+            }
+            _tutorialState = TutorialState.toRelease;
+            tutorialText.text = "Now release the button while that moving sword icon is over the strike area ... the red shape";
+        }
+        while (_tutorialState == TutorialState.toRelease)
+        {
+            while (!(Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.Mouse0)))
+            {
+                yield return null;
+            }
+            TrainingDummy = _enemyManager.aliveEnemys[0];
+            tutorialText.text = "pretty simple right?";
+            _tutorialState = TutorialState.toBlock;
+        }
+        yield return new WaitForSeconds(1.5f);
+        tutorialText.text = "now block this attack";
+        yield return new WaitForSeconds(1f);
+
+        TrainingDummy.StrikeUI();
+        yield return new WaitForSeconds(3f);
+        
+        tutorialText.text = "Lay low your enemy!";
+        List<WeaponEffect> noneEffects = new List<WeaponEffect>();
+        noneEffects.Add(WeaponEffect.none);
+        TrainingDummy.damgEnemy(900, noneEffects);
+
+        yield return new WaitForSeconds(4f);
+        _tutorialState = TutorialState.done;
+
+        if (_tutorialState == TutorialState.done)
+        {
+            _tutorialing = false;
+            tutorialPanel.SetActive(false);
+            //SensaiSprite.SetActive(false);
+        }
+    }
+
+ /*   void Update()
+    {
+        if (_tutorialing)
+        {
+            //SensaiSprite.SetActive(true);
+            tutorialPanel.SetActive(true);
+            tutorialText.text = "Hold Down the Space bar or Left Mouse button";
+            if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Mouse0)) && _tutorialState == TutorialState.tohold)
+            {
+                _tutorialState = TutorialState.toRelease;
+                tutorialText.text = "Now release the button while that moving sword icon is over the strike area ... the red shape";
+                
+            }
+            if ((Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.Mouse0)) && _tutorialState == TutorialState.toRelease)
+            {
+                tutorialText.text = "pretty simple right?";
+                _tutorialState = TutorialState.toBlock;
+            }
+            if (_tutorialState == TutorialState.toBlock)
+            {
+                _timer -= Time.deltaTime;
+                if (_timer >= 1)
+                {
+                    tutorialText.text = "now block this attack";
+                    TrainingDummy.StrikeUI();
+                    _tutorialState = TutorialState.done;
+                }
+            }
+
+            if (_tutorialState == TutorialState.done)
+            {
+                _tutorialing = false;
+                tutorialPanel.SetActive(false);
+                //SensaiSprite.SetActive(false);
+            }
+        }
+    }*/
+
+
+    public void yesToTutorial()
+    {
+        _tutorialing = true;
+        Debug.Log("yes tutorial");
+    }
+}
+public enum TutorialState { tohold,toRelease,toBlock, done}
+
