e11ce801dc85dd9344909ac15b914e222eb14e86
Noah <deremo.noah@gmail.com>
2023-03-21 22:11:30 -0500

    started planning event enemy

---
+++ Assets/Scrripts/EnemyStuff/EventEnemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventEnemy : enemy
+{
+    // a guy who is like hey join our group
+    //this would happen before they start attacking
+
+    //the hostage either kid who joined bandits or classic
+    //well the classic would be before and maybe after
+    //the kid would be just after
+
+    //so maybe a variable for the time when the event is triggered
+    //maybe the event itself
+
+    //it might also need a way to stop other enemies from attacking 
+    //maybe a static variable wait in the start function but also it will need to start as normal if violence is chosen
+    
+}
