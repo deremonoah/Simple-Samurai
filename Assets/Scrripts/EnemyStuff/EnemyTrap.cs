d9dc671947ebdb826f067d4b75d3eca4ba51aedc
Noah <deremo.noah@gmail.com>
2023-07-12 22:01:30 -0500

    added sumo hand
    i should refocuse on refining ui but also finishing the sumo enemy would be nice

--- Assets/Scrripts/EnemyStuff/EnemyTrap.cs
+++ Assets/Scrripts/EnemyStuff/EnemyTrap.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class EnemyTrap : MonoBehaviour
 {
     private PlayerHealthBar playerHP;
+    [SerializeField] float damageFromTrap;
     private void Start()
     {
         playerHP = FindObjectOfType<PlayerHealthBar>();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.name == "strike point")
+        if (other.name == "strike point" && damageFromTrap>0)
         {
             //it is anti armor now
-            playerHP.DamagePlayer(null,10f,2);
+            playerHP.DamagePlayer(null, damageFromTrap, 2);
             Destroy(this.gameObject);
         }
     }
