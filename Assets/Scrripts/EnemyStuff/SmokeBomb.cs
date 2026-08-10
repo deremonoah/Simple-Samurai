2c7f3d8992736a1fbbf450113cab3809247de0ca
Noah <deremo.noah@gmail.com>
2026-08-10 07:36:00 -0500

    fixed smoke bomb
    from refactoring, also made it last longer
    maybe I should have the enemy behavior takes its actions in a sequence? so they always or most of the time somehow do a different thing back to back, ya know?

--- Assets/Scrripts/EnemyStuff/SmokeBomb.cs
+++ Assets/Scrripts/EnemyStuff/SmokeBomb.cs
@@ -7,6 +7,11 @@ public class SmokeBomb : MonoBehaviour
     private float aliveTimer = 12;
     IEnumerator Start()
     {
+        List<List<Transform>> temp = EnemysManager.instance.GetTrapSpawnSpots();
+        int rand = Random.Range(0, temp[1].Count);
+        List<Transform> smokeSpots = temp[1];
+        this.transform.position = smokeSpots[rand].position;
+
         FindObjectOfType<SoundManager>().PlaySound("smoke");
         yield return new WaitForSeconds(aliveTimer);
         Destroy(this.gameObject);
